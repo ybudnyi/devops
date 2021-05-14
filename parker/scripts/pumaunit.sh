1e7788ef281613274da6c54d989e84137a6e3c79
ybudnyi <ybudnyi@gmail.com>
2021-05-14 12:40:11 +0300

    terraform

--- parker/scripts/pumaunit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat puma_startup.txt > /etc/systemd/system/puma.service
-
-systemctl start puma.service
-systemctl enable puma.service
---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = "0.15.3"
+}
+provider "google" {
+  version = "3.67.0"
+
+  project = var.project
+
+  region = var.region
+}
+
+resource "google_compute_instance" "app" {
+  name         = "from-terr"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+  metadata = {
+    ssh-keys = "root:${file(var.public_key_path)}"
+  }
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  connection {
+    type        = "ssh"
+    user        = "root"
+    agent       = false
+    private_key = file("/root/.ssh/terr")
+    host        = google_compute_instance.app.network_interface.0.access_config.0.nat_ip
+  }
+  provisioner "file" {
+    source      = "file/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "file/deploy.sh"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-terrpuma-default"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
